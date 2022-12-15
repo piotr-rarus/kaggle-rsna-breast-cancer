0832d5028e906ae8d636fd28c78b11eee81e1c49
ojqbo <61695389+ojqbo@users.noreply.github.com>
2022-12-16 00:53:12 +0100

    PR fixes
    - np.allclose instead of `abs(a-b) < 1e-9`
    - exponentiation instead of multiplication
    - typehints for torch tensors

--- src/lib/metrics.py
+++ src/lib/metrics.py
@@ -1,27 +1,28 @@
 import numpy as np
+import torch
 from numpy.typing import NDArray
 
 
 def pfbeta(
-    labels: NDArray[np.int_], preds: NDArray[np.float_], beta: float = 1.0
-) -> float:
+    labels: NDArray[np.int_] | torch.Tensor,
+    preds: NDArray[np.float_] | torch.Tensor,
+    beta: float = 1.0,
+) -> float | torch.Tensor:
     preds = preds.clip(0, 1)
     y_true_count = labels.sum()
     ctp = preds[labels == 1].sum()
     cfp = preds[labels == 0].sum()
-    beta_squared = beta * beta
-    c_precision = ctp / (ctp + cfp)
-    c_recall = ctp / y_true_count
+    beta_squared = beta**2
+    c_precision: float | torch.Tensor = ctp / (ctp + cfp)
+    c_recall: float | torch.Tensor = ctp / y_true_count
     if (c_precision + c_recall) == 0:
         # by definition the return value here is 0.0
         # Yet returned in this way, it has the same partial derivatives
         # w.r.t precision and recall as the true pF1 (in the limit
         # as the precision goes to 0 and the recall goes to 0)
-        zero: float = (1 + beta_squared) / beta_squared * c_recall
-        return zero
-    result: float = (
+        return (1 + beta_squared) / beta_squared * c_recall
+    return (
         (1 + beta_squared)
         * (c_precision * c_recall)
         / (beta_squared * c_precision + c_recall)
     )
-    return result
--- src/lib/tests/test_metrics.py
+++ src/lib/tests/test_metrics.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 from src.lib.metrics import pfbeta
 
@@ -8,4 +9,6 @@ def test_pfbeta() -> None:
     predictions = np.array([0.1, 0.1, 0.1, 0.9, 0.9, 0.9])
     score = pfbeta(labels, predictions)
     proper_score = 0.9
-    assert abs(score - proper_score) < 1e-9
+    assert np.allclose(score, proper_score)
+    score = pfbeta(torch.from_numpy(labels), torch.from_numpy(predictions))
+    assert np.allclose(score, proper_score)
