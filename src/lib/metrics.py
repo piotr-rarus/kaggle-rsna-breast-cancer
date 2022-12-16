0b331582eac0ccb60e9c43e14d0b1ded8228c135
ojqbo <61695389+ojqbo@users.noreply.github.com>
2022-12-16 08:06:52 +0100

    PR fixes
    - notebook title changed
    - block comment removed

--- src/lib/metrics.py
+++ src/lib/metrics.py
@@ -16,10 +16,7 @@ def pfbeta(
     c_precision: float | torch.Tensor = ctp / (ctp + cfp)
     c_recall: float | torch.Tensor = ctp / y_true_count
     if (c_precision + c_recall) == 0:
-        # by definition the return value here is 0.0
-        # Yet returned in this way, it has the same partial derivatives
-        # w.r.t precision and recall as the true pF1 (in the limit
-        # as the precision goes to 0 and the recall goes to 0)
+        # returns 0.0 as c_recall == 0
         return (1 + beta_squared) / beta_squared * c_recall
     return (
         (1 + beta_squared)
